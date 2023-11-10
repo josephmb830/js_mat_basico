c874e291058bfb7263d8cdf92168c3a1e2731c7c
josephmb830 <josephmoran@duck.com>
2023-11-10 18:06:45 -0500

    Calculando Descuentos con js

--- descuentos.js
+++ descuentos.js
@@ -3,6 +3,16 @@ function calculateFinalPrice() {
     var price = parseFloat(document.getElementById('price').value);
     var discount = parseFloat(document.getElementById('discount').value);
 
+    if (!price || !discount) {
+        document.getElementById('result').innerHTML = "Por favor llenar el formulario";
+        return;
+    }
+
+    if (discount > 100) {
+        document.getElementById('result').innerHTML = "Descuento no puede ser mayor a 100";
+        return;
+    }
+
     // Calculate final price
     var finalPrice = ((price * (100 - discount)) / 100);
 
